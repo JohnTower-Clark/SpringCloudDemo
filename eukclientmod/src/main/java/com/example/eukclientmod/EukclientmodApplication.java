dd4204022325dde9e7e3b4cbfdc4d962648c66df
JohnTower-Clark <1326723999@qq.com>
2020-10-19 07:00:04 +0800

    CloudProj

---
+++ eukclientmod/src/main/java/com/example/eukclientmod/EukclientmodApplication.java
@@ -0,0 +1,32 @@
+package com.example.eukclientmod;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+
+public class EukclientmodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EukclientmodApplication.class, args);
+    }
+
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hi eurk  " + name + " ,I am Clark from port:" + port;
+    }
+
+
+}
---
+++ eukclientmod/src/test/java/com/example/eukclientmod/EukclientmodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eukclientmod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EukclientmodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
